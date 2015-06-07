6d91195f025faff5c17e864aa520f13a220b473e
janz93 <jan.zaydowicz@googlemail.com>
2015-06-07 13:57:41 +0200

    fix class loading error

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -5,7 +5,9 @@ define( 'ROOT_PATH', dirname( dirname( __FILE__ ) ) . '/' );
 define( 'APP_PATH', ROOT_PATH . 'app/' );
 define( 'CONFIG_PATH', ROOT_PATH . 'config/' );
 define( 'CORE_PATH', ROOT_PATH . 'core/' );
-
 require_once 'dbConfig.php';
+require_once CORE_PATH . 'Autoloader.php';
+require 'vendor/autoload.php';
+
 $autoloader = new Autoloader('Storyteller');
 $autoloader->register();
