b6df03509eb9c0fa413ec6979683fff9d9f8c789
janz93 <jan.zaydowicz@googlemail.com>
2015-09-17 15:32:15 +0200

    add join function. close #10

--- core/db/Delete.php
+++ core/db/Delete.php
@@ -4,9 +4,15 @@ namespace Storyteller\core\db;
 
 class Delete extends Query {
   
-  public function __construct($pdo) {
+  public function __construct($pdo, $table, $where = null) {
     $this->PDOConntector = $pdo;
     $this->stm = self::SQL_DELETE . ' ';
+    $this->from($table);
+    if (!is_null($where)) {
+      $this->stm .= $where->stm;
+      $this->_isWhereExsists = $where->_isWhereExsists;
+      $this->_bind = $where->_bind;
+    }
   }
    
   public function finalQuery() {
