570ba96817710f4f6c18ebf61b90353f2fd66a1e
janz93 <jan.zaydowicz@googlemail.com>
2015-10-14 01:07:36 +0200

    add update function. close #9

--- core/db/Update.php
+++ core/db/Update.php
@@ -4,12 +4,57 @@ namespace Storyteller\core\db;
 
 class Update extends Query {
   
-  public function __construct($pdo, $data) {
+  const WHERE_PATTERN = "/\?/";
+  
+  public function __construct($pdo, $table, $data, $where) {
     $this->PDOConntector = $pdo;
-    $this->stm = self::SQL_UPDATE . ' ';
+    $this->stm = self::SQL_UPDATE . ' `' . $table->info() . '` ' . self::SQL_UPDATE_SET . ' ';
+    $this->stm .= $this->_addData($data);
+    if (!is_null($where)) {
+      if($this->_hasAnymoousWhereCondition($where)) {
+        $this->_replaceAnynimousWhereCondition($where);
+      }
+      $this->stm .= $where->stm;
+      $this->_isWhereExsists = $where->_isWhereExsists;
+      $this->_bind += $where->_bind;
+    }
   }
    
   public function finalQuery() {
     return parent::finalQuery($this->PDOConntector);
+  }
+  
+  private function _addData($data) {
+    $queryStr = '';
+    $i = 1;
+    foreach ($data as $column => $value) {
+      $queryStr .= '`' . $column . '` = :' . $column;
+      $queryStr .= ($i > count($data)) ? ', ' : ' ';
+      $this->_bind += array(':' . $column => $value);      
+      $i++;
+    }
+    return $queryStr;
+  }
+  
+  private function _hasAnymoousWhereCondition($where) {
+    $result = strpos($where->stm, '?');
+    if ($result) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  private function _replaceAnynimousWhereCondition($where) {
+    $uniqueIds = array();
+    for ($i = 1; $i <= count($where->_bind); $i++) {
+      $uniqueId = ':' . uniqid();
+      $where->_bind += array($uniqueId => $where->_bind[$i]);
+      array_push($uniqueIds, $uniqueId);
+      unset($where->_bind[$i]);
+    }
+    $replacements = $uniqueIds;
+    $search_array = array_fill(0, sizeof($replacements), self::WHERE_PATTERN);
+    $where->stm = preg_replace($search_array, $replacements, $where->stm, 1);
   }
 }
